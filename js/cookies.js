76f5059ef4dff068e340ea67a8aa40c60a049ef8
ray70607.github.io <garfield786@126.com>
2020-12-20 11:00:06 +0800

    Create cookies.js

---
+++ js/cookies.js
@@ -0,0 +1,14 @@
+function createCookie(name,value,days,path,domain,secure) {
+    var expires='';
+    if (days){
+        var date=new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        expires=date.toGMTString();
+    }
+    var cookieString= name+'='+escape(value);
+    if (expires)cookieString+=';expires='+expires;
+    if (path)cookieString+=';path='+path;
+    if(domain)cookieString+=';domain='+domain;
+    if (secure)cookieString+=';secure'+secure;
+    document.cookie=cookieString;
+}
