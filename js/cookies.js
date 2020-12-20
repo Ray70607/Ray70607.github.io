abe6c860ca20825fa5bc552dd403626eb7f7546c
ray70607.github.io <garfield786@126.com>
2020-12-20 11:11:39 +0800

    Update cookies.js

--- js/cookies.js
+++ js/cookies.js
@@ -5,7 +5,7 @@ function createCookie(name,value,days,path,domain,secure) {
         date.setTime(date.getTime()+(days*24*60*60*1000));
         expires=date.toGMTString();
     }
-    var cookieString= name+'='+escape(value);
+    cookieString= name+'='+escape(value);
     if (expires)cookieString+=';expires='+expires;
     if (path)cookieString+=';path='+path;
     if(domain)cookieString+=';domain='+domain;
