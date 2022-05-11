da6f67843f022a98fd79b63b3b44f75c0f200f6e
ray70607.github.io <garfield786@126.com>
2022-05-11 14:33:10 +0800

    new clock

---
+++ temp/luopanshizhong/js/config.js
@@ -0,0 +1,14 @@
+var config={
+
+    'language_type':2,
+    'font_color':'#6666CC',
+    'pointer_color':'#CCFFFF',
+
+    /*'sound':0,
+    'sound_name':'cc.mp3',
+
+    'background_style':1,
+    'background_picture':'bg.jpg',
+    'background_video':'3.mp4',
+    'background_color':'#000000',*/
+}
